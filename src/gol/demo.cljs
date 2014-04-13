bad8c66acb5188abc9247ba8261697397276a28b
Brent Shields <shieldsbrent@gmail.com>
2014-04-13 12:40:36 +0200

    Working implementation, needs some cleanup

--- src/gol/demo.cljs
+++ src/gol/demo.cljs
@@ -2,14 +2,14 @@
   (:require [rxatom.core :as rx]
             [cljs.reader :as reader]))
 
-(def grid-x 100)
-(def grid-y 100)
-(def cell-size 10)
+(def ^:export grid-x 30)
+(def ^:export grid-y 30)
+(def ^:export cell-size 20)
+
 (defn cell-color [set?] (if set? "black" "none"))
 
 (def grid-indices (for [y (range grid-y) x (range grid-x)]
                     [x y]))
-
 
 (defn cell-svg [[x y :as idx]]
   (str "<rect id="
@@ -19,45 +19,76 @@
        " width=" cell-size
        " height=" cell-size
        " stroke=black fill=none stroke-width=2px pointer-events=fill"
-       " onclick='gol.demo.flip_cell(this.id); gol.demo.step()'/>"))
+       " onclick='gol.demo.flip_cell(this.id);'/>"))
 
-(defn grid-height []
-  (* grid-y cell-size))
-
-(defn grid-width []
-  (* grid-x cell-size))
-
-(defn grid-svg []
+(defn ^:export grid-svg []
   (apply str (map cell-svg grid-indices)))
 
 
 (def grid (rx/rxatom (zipmap grid-indices (repeat false))))
 
-(def cells
-  (zipmap grid-indices (map #(rx/rxlens-key grid %) grid-indices)))
+(def cells (zipmap grid-indices (map #(rx/rxlens-key grid %) grid-indices)))
 
-
-(def history (atom '()))
-
-(defn step-back []
-  (when-not (empty? @history)
-    (reset! grid (first @history))
-    (swap! history next)
-    (rx/commit-frame! grid)))
-
-
-(defn flip-cell [coord-string]
-  (let [idx (reader/read-string coord-string)]
-    (swap! (cells idx) not)))
-
-(defn step []
-  (swap! history conj @grid)
-  (rx/commit-frame! grid))
-
-(defn observe-cell [idx]
+(defn observe-cell-display [idx]
   (rx/observe (cells idx)
               #(-> js/document
                    (.getElementById (pr-str idx))
                    (.setAttribute "fill" (cell-color %)))))
 
-(def observers (doall (map observe-cell grid-indices)))
+(def cell-renderers (doall (map observe-cell-display grid-indices)))
+
+(def gen-counter (rx/rxatom 0))
+
+(defn neighbors [[x y]]
+  (for [dx [-1 0 1]
+        dy [-1 0 1]
+        :let [neighbor [(mod (+ x dx) grid-x) (mod (+ y dy) grid-y)]]
+        :when (not= neighbor [x y])]
+    neighbor))
+
+(defn alive-next-gen? [cell-alive? n0 n1 n2 n3 n4 n5 n6 n7]
+  (let [alive-neighbors (count (filter identity [n0 n1 n2 n3 n4 n5 n6 n7]))]
+    (if cell-alive?
+      (#{2 3} alive-neighbors)
+      (= 3 alive-neighbors))))
+
+(defn next-gen-cell [idx]
+  (let [[c n0 n1 n2 n3 n4 n5 n6 n7]
+        (->> idx neighbors (cons idx) (map cells) vec)]
+    (rx/rxfn c n0 n1 n2 n3 n4 n5 n6 n7 alive-next-gen?)))
+
+(def history (atom '()))
+
+(defn update-grid []
+  (swap! history conj @grid)
+  (rx/commit-frame! grid))
+
+(defn ^:export step-back []
+  (when-not (empty? @history)
+    (reset! grid (first @history))
+    (swap! history next)
+    (rx/commit-frame! grid)))
+
+(defn ^:export flip-cell [coord-string]
+  (let [idx (reader/read-string coord-string)]
+    (swap! (cells idx) not)
+    (update-grid)))
+
+(defn ^:export next-gen []
+  (swap! gen-counter inc)
+  (rx/commit-frame! gen-counter)
+  (update-grid))
+
+(rx/observe gen-counter #(.log js/console (str "Gen Incremented: " %)))
+
+(defn gen-updater [idx]
+  (let [cell (cells idx)
+        last-gen (atom 0)]
+    (rx/observe (rx/rxfn cell (next-gen-cell idx) gen-counter vector)
+                (fn [[cur next-gen gen-count]]
+                   (when (and (> gen-count @last-gen)
+                              (not= cur next-gen))
+                     (reset! cell next-gen))
+                   (reset! last-gen gen-count)))))
+
+(def next-gen-updaters (doall (map gen-updater grid-indices)))
